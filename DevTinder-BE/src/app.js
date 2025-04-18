469e9ff4e1d674f36357079810458b22243c9e1d
pawanmishra1728 <pawanthp17@gmail.com>
2025-04-18 10:25:54 +0530

    Schema validation

--- DevTinder-BE/src/app.js
+++ DevTinder-BE/src/app.js
@@ -4,31 +4,49 @@ const { connectDB } = require("../src/config/database");
 const { User } = require("./models/user");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send("user  get successfully");
-});
-
-app.use(express.json())
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    console.log(req.body)
-//   let user = {
-   
-//     lastName: "Mishra",
-//     age: 32,
-//     emailId: "emailId",
-//     password: "12345678",
-//     gender: "male",
-//   };
+  console.log(req.body);
   const newUser = new User(req.body);
-  
+
   try {
     let msg = await newUser.save();
-  res.send(`User ${msg.firstName} added successfully`);
+    res.send(`User ${msg.firstName} added successfully`);
   } catch (error) {
-    console.log(error,error.message)
-    res.status(400).send(`error is ${error._message} `)
+    console.log(error, error.message);
+    res.status(400).send(`error is ${error._message} `);
   }
+});
+
+app.post("/user", async (req, res) => {
+  let newData = await User.create(req.body);
+  newData.save();
+  res.send("user added successfully");
+});
+app.patch("/user", async (req, res) => {
+  let data = req.body;
+  console.log("🚀 ~ app.patch ~ data:", data)
+  try {
+    let user = await User.findByIdAndUpdate({_id :data._id}, data, {
+      runValidators: true,
+    });
+
+    res.send("UPDATE SUCESSFULLY");
+  } catch (error) {
+    res.status(400).send("Something went wrong" + error.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  let newData = await User.findOneAndDelete({ _id: req.body });
+  res.send("user deleted successfully");
+});
+
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.emailId;
+  const user = await User.find({ emailId: userEmail });
+  res.send(`user  get successfully ${user}`);
 });
 
 connectDB()
@@ -36,4 +54,4 @@ connectDB()
     console.log("Database connected");
     app.listen(7777);
   })
-  .catch((err) => console.log("error occure"));
+  .catch((err) => console.log("error occure", err.message));
--- DevTinder-BE/src/models/user.js
+++ DevTinder-BE/src/models/user.js
@@ -1,25 +1,45 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength:3,
+      maxLength:50
+    },
+    lastName: {
+      type: String,
+      maxLength:50
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require:true
+    },
+    age: {
+      type: Number,
+      min:18
+    },
+    gender: {
+      type: String,
+      validate(value){
+        if(!["male","female","other"].includes(value)){
+          throw new Error("Gender is not valid")
+        }
+      }
+    },
+    skills:{
+      type:[String],
+      default: ["Javascript"]
+    }
   },
- lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
-const User = mongoose.model("User",userSchema)
-module.exports ={User}
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
